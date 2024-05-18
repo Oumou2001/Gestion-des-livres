d2c2c33d4b68b7a330eebe9391fe45c4102f8456
Oumou2001 <167223247+Oumou2001@users.noreply.github.com>
2024-05-18 15:02:11 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,76 @@
 
 
-
+let alire = document.querySelector("#alire");
 let modal = document.getElementById("elementModal");
 let modalButton = document.getElementById("buttonModal");
 let close = document.querySelector(".close");
 let lu = document.getElementById("lu");
-
-modalButton.onclick = function () {
- modal.style.display = 'block';
-}
-close.onclick = function () {
-    modal.style.display = 'none';
-   }
-   
-
-// const Livre = document.querySelector(".nomLivre");
-// const Auteur= document.querySelector(".nomAuteur");
 let buttonAjouter = document.querySelector(".buttonSubmit");
 const titleElement = document.querySelector(".title");
+const livres = JSON.parse(localStorage.getItem('livres')) || [];
 
 
-buttonAjouter.addEventListener ("click", function()
+modalButton.onclick = function () {
+  modal.style.display = 'block';
+}
+close.onclick = function () {
+  modal.style.display = 'none';
+}
+buttonAjouter.onclick = function () {
+  modal.style.display = 'none';
+}
 
-{
-    // const name = document.createElement("h2");
 
-    // event.preventDefault();
-    // // const titre = document.createElement("h2");
-    // name.innerHTML =`<p> ${Livre.value} ${Auteur.value} </p> ` ;
-    // titleElement.appendChild(name);
-      
-    // console.log("buttonAjouter", buttonAjouter);
-    // 
-let titre = document.getElementById("titre").value;
-let Auteur = document.getElementById("Auteur").value;   
-// alire.innerHTML += `<li>${titre}</li>`;
-alire.insertAdjacentHTML("afterbegin",`<li>${titre}<i id=${titre} onClick=supprimer(this.id) class="fa-solid fa-trash"></i><i id=${titre} class="fa-solid fa-check" onClick=marquerLu(this.id)></i></li>
+
+buttonAjouter.addEventListener("click", function () {
+
+  let titre = document.getElementById("titre").value;
+  let auteur = document.getElementById("Auteur").value;
+  alire.insertAdjacentHTML("afterbegin", `<li  id="${titre}-${auteur}">${titre} ${auteur} <i  
+onClick=supprimer("${titre}-${auteur}") class="fa-solid fa-trash"></i><i  class="fa-solid fa-check" 
+onClick=marquerLu("${titre}-${auteur}")></i></li>
 </li>`);
 
-}
-)
+  const livre = {
+    titre: titre,
+    auteur: auteur,
 
-function supprimer(id)
-{
-    document.getElementById(id).parentElement.outerHTML="";
+  }
+  livres.push(livre);
+
+  localStorage.setItem('livres', JSON.stringify(livres));
+
+})
+
+
+
+function supprimer(id) {
+  const el = document.getElementById(id)
+  el.parentElement.removeChild(el);
 }
 
-function marquerLu(id)
-{
-   
+function marquerLu(id) {
+
   supprimer(id);
-lu.insertAdjacentHTML("afterbegin",`<li>${id}<i id=${id}  onClick=supprimer(this.id) class="fa-solid fa-trash"></i></li>`);
-  
+  lu.insertAdjacentHTML("afterbegin", `<li>${id.split("-")[0]} ${id.split("-")[1]}<i id=${id}  
+onClick=supprimer("${id}") class="fa-solid fa-trash"></i></li>`);
+
 }
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  livres.forEach(livre => {
+
+ 
+  let titre = document.getElementById("titre").value;
+  let auteur = document.getElementById("Auteur").value;
+  alire.insertAdjacentHTML("afterbegin", `<li  id="${livre.titre}-${livre.auteur}">${livre.titre} ${livre.auteur} <i  
+onClick=supprimer("${livre.titre}-${livre.auteur}") class="fa-solid fa-trash"></i><i  class="fa-solid fa-check" 
+onClick=marquerLu("${livre.titre}-${livre.auteur}")></i></li>
+</li>`);
+})
+  
+})
